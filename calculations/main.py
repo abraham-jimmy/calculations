038dad6938fdf218056b6301e7652050a5240983
cMneth <abrahamjimmy@hotmail.com>
2020-07-27 20:40:53 +0200

    my first child

---
+++ calculations/main.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+
+plt.figure()
+plt.plot([1, 2, 3])
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+
+
+setup(
+    name='calculations',
+    version='0.1',
+    packages=find_packages(),
+    license='MIT license',
+    author_email='',
+    author='Jimmy',
+    description='I calculate'
+)

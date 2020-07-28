f1ce9111ee716b5db88805b69bc3bd81d20e0932
cMneth <abrahamjimmy@hotmail.com>
2020-07-29 00:16:31 +0200

    WE DID IT BOYZZ

--- calculations/main.py
+++ calculations/main.py
@@ -2,13 +2,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-
-
 # def plottaren(funktion, start, slut, noggrannhet):
 #     x1 = np.linspace(start, slut, noggrannhet)
 #     y = np.zeros(noggrannhet)
-    for i, x in enumerate(x1):
-        y[i] = eval(funktion)
+#     for i, x in enumerate(x1):
+#         y[i] = eval(funktion)
 #     plt.figure()
 #     plt.plot(y)
 #     plt.show()
@@ -17,24 +15,30 @@ import numpy as np
 # plottaren("x**2", 0, 10, 100)
 
 
-
-def plottaren2(funktion, start, slut, noggrannhet):
+def plottaren(funktion, start, slut, noggrannhet):
     x = np.linspace(start, slut, noggrannhet)
-    y = funktion
+    y = eval(funktion)
+    print(funktion)
     fig = plt.figure(figsize=(10, 5))
     # Add a title
     plt.title('En funktion')
     # Add X and y Label
-    plt.ylabel('Tid')
+    plt.xlabel('Tid')
     plt.ylabel('Sträcka')
-    plt.plot(x, y, 'g', '-.')
+    plt.plot(x, y, 'g', label=format(funktion))
     plt.legend()
+    plt.grid(True)
     plt.show()
 
 
-plottaren2('x ** 2', 0, 10, 100)
+plottaren("1/(2 * np.pi)**0.5 * np.exp(-x**2 / 2)", 0, 10, 100)
 
 
 
 
-# y = 1/(2 * np.pi)**0.5 * np.exp(-x**2 / 2)
+
+
+
+
+
+

2bab44f74a07b4add32ff394c67b13a929fa6847
cMneth <abrahamjimmy@hotmail.com>
2020-08-01 14:21:29 +0200

    Legend is working ~ ish. fint

--- calculations/main.py
+++ calculations/main.py
@@ -3,34 +3,40 @@ import numpy as np
 from inspect import getsourcelines
 
 
-# def plottaren(funktion, start, slut, noggrannhet):
-#     x1 = np.linspace(start, slut, noggrannhet)
-#     y = np.zeros(noggrannhet)
-#     for i, x in enumerate(x1):
-#         y[i] = eval(funktion)
-#     plt.figure()
-#     plt.plot(y)
-#     plt.show()
-#
-#
-# plottaren("x**2", 0, 10, 100)
+# noggrannhet=int(input("noggrannhet:"))
 
 
-def plottaren(funktion, start, slut, noggrannhet):
+def plottaren(funktion, start=0, slut=5, noggrannhet=1000, plot=True, block=True, legend=None):
     x = np.linspace(start, slut, noggrannhet)
-    y = funktion(x)
-    fig = plt.figure(figsize=(10, 5))
-    plt.title('En funktion')
-    plt.xlabel('Tid')
-    plt.ylabel('Sträcka')
-    plt.plot(x, y, 'g', label=getsourcelines(funktion))
-    plt.legend()
-    plt.grid(True)
-    plt.show()
+
+    if funktion.__class__.__name__ == "str":
+        y = eval(funktion)
+        label = format(funktion)
+
+    elif funktion.__class__.__name__ == "function":
+        y = funktion(x)
+        label = getsourcelines(funktion)
+
+    else:
+        raise AttributeError(f"Got {funktion} but this is not a parameter nor a string")
+
+    if legend is not None:
+        label = legend
+
+    if plot:
+        plt.figure(figsize=(10, 5))
+        plt.title('En funktion')
+        plt.xlabel('Tid')
+        plt.ylabel('Sträcka')
+        plt.plot(x, y, 'g', label=label)
+        plt.legend()
+        plt.grid(True)
+        plt.show(block=block)
+
+    return y
 
 
-def ekvation(x):
-    return np.sin(x ** 3 / 5) * 20
+plottaren("x**2", block=False)
+plottaren(lambda x: x ** 2, legend="x^2")
 
 
-plottaren(lambda x: ekvation(x), 0, 10, 1000)
